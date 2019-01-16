29bc3062f5572a2f1b087a16df6443200c7b1b67
Vladimir Krejcik <zadeeek@gmail.com>
2019-01-16 10:43:13 +0100

    trying to prepare master.pp for different debian versions

--- manifests/master.pp
+++ manifests/master.pp
@@ -3,14 +3,28 @@ class puppetnode::master(
   $server_jvm_min_heap_size = '2048m'
   ) {
 
+  if $facts['operatingsystemmajrelease'] == '8' {
+    $puppet_package_version = '1.10.1-1jessie',
+    $puppet_collections     = 'jessie'
+  }
+  elsif $facts['operatingsystemmajrelease'] == '9' {
+    $puppet_package_version = 'puppet5-release-stretch',
+    $puppet_collections     = 'stretch'
+  }
+  else {
+    # default - can be anything
+    $puppet_package_version = '1.10.1-1jessie',
+    $puppet_collections     = 'jessie'
+  }
+
   #install collection repo
   exec { 'install-collection':
-    command => 'wget https://apt.puppetlabs.com/puppetlabs-release-pc1-jessie.deb;dpkg -i puppetlabs-release-pc1-jessie.deb',
+    command => "wget https://apt.puppetlabs.com/puppetlabs-release-pc1-${puppet_collections}.deb;dpkg -i puppetlabs-release-pc1-${$puppet_collections}.deb",
     user    => 'root',
     path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
     creates => '/tmp/puppetlabs-release-pc1-jessie.deb',
     cwd     => '/tmp/',
-    require => Package['wget']
+    require => Package['wget', 'ca-certificates']
   }
 
 # file { '/var/lib/puppet':
@@ -18,7 +32,6 @@ class puppetnode::master(
 #  target => '/opt/puppetlabs/puppet',
 #  before => Class['::puppet']
 #}
-
 
   file { '/etc/puppetserver':
     ensure => link,
@@ -38,7 +51,6 @@ class puppetnode::master(
 #  before => Class['::puppet']
 #}
 
-
   class { '::puppet':
     server                      => true,
     server_git_repo             => false,
@@ -48,7 +60,7 @@ class puppetnode::master(
     server_reports              => 'puppetdb',
     server_storeconfigs_backend => 'puppetdb',
     server_implementation       => 'puppetserver',
-    version                     => '1.10.1-1jessie',
+    version                     => $puppet_package_version,
     server_version              => '2.7.2-1puppetlabs1',
     server_puppetserver_version => '2.7.2',
     server_jvm_min_heap_size    => '2048m',
